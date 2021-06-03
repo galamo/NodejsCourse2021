3974651f0b4881282b59f6b6be30dc9074e77b8a
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2021-06-03 13:06:16 +0300

    first_lesson

---
+++ async/index.js
@@ -0,0 +1,21 @@
+
+async function getFromServer() {
+    console.log(4444)
+    const result = await fetch("http://ynet.co.il")
+    console.log(1234)
+
+}
+async function getFromServer2() {
+    console.log(6666)
+    const result = await fetch("http://google.co.il")
+    console.log(8888)
+
+}
+console.log(9999)
+getFromServer2()
+getFromServer()
+console.log(5555)
+
+// Yonatan
+9999 > 6666 > 4444 > 5555 > 8888 || 1234
+
