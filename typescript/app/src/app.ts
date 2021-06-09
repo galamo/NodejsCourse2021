54127d1b5f5ccc651a2bf7870299616d5109a207
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2021-06-10 00:06:55 +0300

    ts

--- typescript/app/src/app.ts
+++ typescript/app/src/app.ts
@@ -1,6 +1,32 @@
-const a = 1;
-const aaaaa4 = 1;
-const a3 = 1;
-const a2 = 1;
-const a1 = 1;
-console.log(a)
+import values from "./values.json";
+
+const [firstCar] = values;
+
+type Car = typeof firstCar;
+
+type Cr = { y: number }
+interface ICar extends Cr {
+    y: number
+}
+
+function addCar(car: Car) {
+    console.log([...values, car])
+
+}
+addCar({
+    "Name": "chevrolet chevelle malibu",
+    "Miles_per_Gallon": 18,
+    "Cylinders": 8,
+    "Displacement": 307,
+    "Horsepower": 130,
+    "Weight_in_lbs": 3504,
+    "Acceleration": 12,
+    "Year": "1970-01-01",
+    "Origin": "USA"
+})
+
+
+interface IProduct {
+    name: string,
+    price: number,
+}
